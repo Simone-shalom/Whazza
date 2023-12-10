e8674b79685cb67ec3274bc733bfe4e46c6c8256
unknown <studzinskis46@gmail.com>
2023-12-10 13:23:34 +0100

    feat: Working Husky

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,33 +1,31 @@
+import { LogoComp } from "@/components/LogoComp";
+import MobileSidebar from "@/components/MobileSidebar";
+import Navbar from "@/components/Navbar";
+import Link from "next/link";
+import React from "react";
 
-
-import { LogoComp } from '@/components/LogoComp'
-import MobileSidebar from '@/components/MobileSidebar'
-import Navbar from '@/components/Navbar'
-import Link from 'next/link'
-import React from 'react'
-
-const RoutesLayout = ({children}: {children: React.ReactNode}) => {
-
-var
-
+const RoutesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className=''>
-      <div className='hidden md:block'>
+    <div className="">
+      <div className="hidden md:block">
         <Navbar />
       </div>
-      <div className='fixed w-full z-50'>
-        <div className={`md:hidden flex bg-gradient-to-b from-purple-100 to-purple-50  justify-between py-4 px-4 rounded-b-xl`}>
-        <Link href={"/"} className="text-2xl  font-semibold text-black hover:opacity-90">
+      <div className="fixed w-full z-50">
+        <div
+          className={`md:hidden flex bg-gradient-to-b from-purple-100 to-purple-50  justify-between py-4 px-4 rounded-b-xl`}
+        >
+          <Link
+            href={"/"}
+            className="text-2xl  font-semibold text-black hover:opacity-90"
+          >
             <LogoComp />
           </Link>
           <MobileSidebar />
         </div>
       </div>
-      <main className=''>
-        {children}
-      </main>
+      <main className="">{children}</main>
     </div>
-  )
-}
+  );
+};
 
-export default RoutesLayout
+export default RoutesLayout;
