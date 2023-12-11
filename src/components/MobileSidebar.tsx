3f0f99977125aee6c87f19747d8e74ac6b639102
unknown <studzinskis46@gmail.com>
2023-12-11 16:13:33 +0100

    feat:Navabr/Sidebar better bg contrast color

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -1,80 +1,87 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
-import { Button } from "./ui/button"
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
-import {Menu} from 'lucide-react'
-import Link from "next/link"
-import { useSession } from "next-auth/react"
-import AnimatedBlob from "./AnimatedBlob"
-import SelectedBadge from "./SelectedBadge"
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import AnimatedBlob from "./AnimatedBlob";
+import SelectedBadge from "./SelectedBadge";
 
-interface MobileSidebarProps {
-  
-}
+interface MobileSidebarProps {}
 
 const MobileSidebar = ({}: MobileSidebarProps) => {
+  const { data: session } = useSession();
+  const [mounted, setMounted] = useState(false);
 
-    const { data: session } = useSession();
-    const [mounted, setMounted] = useState(false)
-
-    
   useEffect(() => {
+    setMounted(true);
+  }, []);
 
-    setMounted(true)
-
-  },[])
-
-  if(!mounted) {
-    return null
+  if (!mounted) {
+    return null;
   }
 
   const generateDashboardLink = () => {
-    const callbackUrl = '/dashboard'; // Specify your desired dashboard URL
-    const customSignInPage = '/auth'; // Path to your custom sign-in page
+    const callbackUrl = "/dashboard"; // Specify your desired dashboard URL
+    const customSignInPage = "/auth"; // Path to your custom sign-in page
 
     if (session) {
       return callbackUrl;
     } else {
       // Generate the link to your custom sign-in page with callbackUrl
-      return `${customSignInPage}?callbackUrl=${encodeURIComponent(callbackUrl)}`;
+      return `${customSignInPage}?callbackUrl=${encodeURIComponent(
+        callbackUrl
+      )}`;
     }
   };
 
-
   return (
-   <Sheet>
-    <SheetTrigger>
-      <Button data-testid="MenuBtn" variant='ghost' className="md:hidden hover:bg-transparent hover:scale-125 transition duration-300" aria-label="Mobile Menu">
-        <Menu size={20}/>
-      </Button>
-    </SheetTrigger>
-    <SheetContent side='right' className="p-0 w-64 bg-purple-100/90">
-        <div data-testid="LinksContainer" className="flex flex-col items-center gap-10 pt-20 cursor-pointer">
-        <Link href={generateDashboardLink()} className="font-medium text-sm text-white bg-black px-4 py-2 rounded-lg 
-          hover:opacity-80 hover:scale-110 transition duration-200">
+    <Sheet>
+      <SheetTrigger>
+        <Button
+          data-testid="MenuBtn"
+          variant="ghost"
+          className="md:hidden hover:bg-transparent hover:scale-125 transition duration-300"
+          aria-label="Mobile Menu"
+        >
+          <Menu size={20} />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="right" className="p-0 w-64 bg-purple-50/90">
+        <div
+          data-testid="LinksContainer"
+          className="flex flex-col items-center gap-10 pt-20 cursor-pointer"
+        >
+          <Link
+            href={generateDashboardLink()}
+            className="font-medium text-sm text-white bg-black px-4 py-2 rounded-lg 
+          hover:opacity-80 hover:scale-110 transition duration-200"
+          >
             Dashboard
           </Link>
-          <Link href="/events" className="font-medium text- text-black hover:opacity-80 hover:scale-110 transition duration-200">
+          <Link
+            href="/events"
+            className="font-medium text- text-black hover:opacity-80 hover:scale-110 transition duration-200"
+          >
             Events
           </Link>
-          <Link href="/pricing" className="font-medium text- text-black hover:opacity-80 hover:scale-110 transition duration-200">
+          <Link
+            href="/pricing"
+            className="font-medium text- text-black hover:opacity-80 hover:scale-110 transition duration-200"
+          >
             Pricing
           </Link>
-          {session && (
-            <SelectedBadge/>
-          )}
+          {session && <SelectedBadge />}
           <div className="pt-64 w-full">
-            <AnimatedBlob>
-            </AnimatedBlob>  
+            <AnimatedBlob></AnimatedBlob>
           </div>
-          <p className="pt-64">
-            Simone s
-          </p>
+          <p className="pt-64">Simone s</p>
         </div>
-    </SheetContent>
-   </Sheet>
-  )
-}
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default MobileSidebar
+export default MobileSidebar;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,76 +1,89 @@
-'use client'
+"use client";
 
 import Link from "next/link";
-import { useSession } from 'next-auth/react';
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import SelectedBadge from "./SelectedBadge";
 import { LogoComp } from "./LogoComp";
 
- const Navbar = ()=> {
+const Navbar = () => {
   const { data: session } = useSession();
-  const [showBackground, setShowBackground] = useState(false)
-
+  const [showBackground, setShowBackground] = useState(false);
 
   const generateDashboardLink = () => {
-    const callbackUrl = '/dashboard'; // Specify your desired dashboard URL
-    const customSignInPage = '/auth'; // Path to your custom sign-in page
+    const callbackUrl = "/dashboard"; // Specify your desired dashboard URL
+    const customSignInPage = "/auth"; // Path to your custom sign-in page
 
     if (session) {
       return callbackUrl;
     } else {
       // Generate the link to your custom sign-in page with callbackUrl
-      return `${customSignInPage}?callbackUrl=${encodeURIComponent(callbackUrl)}`;
+      return `${customSignInPage}?callbackUrl=${encodeURIComponent(
+        callbackUrl
+      )}`;
     }
   };
 
-  const TOP_OFFSET = 66
+  const TOP_OFFSET = 66;
 
   useEffect(() => {
     const handleScroll = () => {
-        if(window.scrollY > TOP_OFFSET) {
-            setShowBackground(true)
-    }else {
-        setShowBackground(false)
-    }
-}
-window.addEventListener('scroll', handleScroll)
+      if (window.scrollY > TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
 
-return() =>{
-    window.removeEventListener('scroll', handleScroll)
-}
-
-},[])
-
-
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <nav data-testid="navbar-container" className= {`  ${showBackground ? ' bg-gradient-to-b from-white to-purple-100' : ''} 
-      w-full px-4 fixed z-50 shadow-md `}>
-      <div className={`flex  items-center gap-8 px-5 rounded-xl justify-between 
-        py-3 max-w-7xl m-auto transition duration-500 h-24`}>
-        <Link href={"/"} className="text-2xl font-semibold text-black hover:opacity-80">
+    <nav
+      data-testid="navbar-container"
+      className={`  ${
+        showBackground ? " bg-gradient-to-b from-white to-purple-50" : ""
+      } 
+      w-full px-4 fixed z-50 shadow-md `}
+    >
+      <div
+        className={`flex  items-center gap-8 px-5 rounded-xl justify-between 
+        py-3 max-w-7xl m-auto transition duration-500 h-24`}
+      >
+        <Link
+          href={"/"}
+          className="text-2xl font-semibold text-black hover:opacity-80"
+        >
           <LogoComp />
         </Link>
         <div className="flex items-center gap-4">
-          <div data-testid="selected-badge">
-            {session && (
-            <SelectedBadge/>
-            )}
-          </div>
-          <Link href="/events" className="font-medium text-lg  hover:scale-110 transition duration-200 text-black hover:opacity-80">
+          <div data-testid="selected-badge">{session && <SelectedBadge />}</div>
+          <Link
+            href="/events"
+            className="font-medium text-lg  hover:scale-110 transition duration-200 text-black hover:opacity-80"
+          >
             Events
           </Link>
-          <Link href="/pricing" className="font-medium text-lg  hover:scale-110 transition duration-200 text-black hover:opacity-90">
+          <Link
+            href="/pricing"
+            className="font-medium text-lg  hover:scale-110 transition duration-200 text-black hover:opacity-90"
+          >
             Pricing
           </Link>
-          <Link href={generateDashboardLink()} className="font-medium text-sm text-white bg-black px-4 py-2 
-            rounded-lg hover:opacity-90  hover:scale-110 transition duration-200">
+          <Link
+            href={generateDashboardLink()}
+            className="font-medium text-sm text-white bg-black px-4 py-2 
+            rounded-lg hover:opacity-90  hover:scale-110 transition duration-200"
+          >
             Dashboard
           </Link>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
