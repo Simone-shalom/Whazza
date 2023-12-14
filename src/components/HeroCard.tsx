5dc0a67be9785f326e0b3aa8c452fe05df832b50
unknown <studzinskis46@gmail.com>
2023-12-14 14:29:03 +0100

    feat:Small componnets for dashboard updated

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -1,33 +1,31 @@
-import Image from "next/image"
-import AnimatedBlob from "./AnimatedBlob"
-
+import Image from "next/image";
+import AnimatedBlob from "./AnimatedBlob";
 
 interface HeroCardProps {
-    src: string
-    right?: boolean
+  src: string;
+  right?: boolean;
 }
 
-export const HeroCard = ({src, right}: HeroCardProps) => {
-
+export const HeroCard = ({ src, right }: HeroCardProps) => {
   return (
     <>
-    {right ? (
-        <div className="hidden xl:block absolute right-12 top-64 overflow-x-clip">
-        <AnimatedBlob>
-          <div>
-          <Image src={src} alt="" width={250} height={200}/>
-          </div>
-        </AnimatedBlob>
+      {right ? (
+        <div className="hidden xl:block absolute right-12 top-80 overflow-x-clip">
+          <AnimatedBlob>
+            <div>
+              <Image src={src} alt="" width={250} height={200} />
+            </div>
+          </AnimatedBlob>
         </div>
-    ): (
-        <div className="hidden xl:block absolute left-12 top-64 overflow-x-clip">
-        <AnimatedBlob>
-          <div>
-          <Image src={src} alt="" width={250} height={200}/>
-          </div>
-        </AnimatedBlob>
+      ) : (
+        <div className="hidden xl:block absolute left-12 top-80 overflow-x-clip">
+          <AnimatedBlob>
+            <div>
+              <Image src={src} alt="" width={250} height={200} />
+            </div>
+          </AnimatedBlob>
         </div>
-    ) }
+      )}
     </>
-  )
-}
+  );
+};
--- src/components/Scoring.tsx
+++ src/components/Scoring.tsx
@@ -1,27 +1,28 @@
-'use client'
-
+"use client";
 
 const Scoring = () => {
   return (
-       <div className="flex flex-col z-20 items-center justify-center">
-                   
-                    <h3 className="text-3xl xl:text-4xl font-semibold pt-3  text-orange-700">Scoring system</h3>
-                    <p className="text-xl text-gray-800 font-semibold capitalize">only the best result counts</p>
-                 
-                  <div className="text-xl pt-3 ">
-                    <p className="pt-1">
-                    1 place in event = <span className="font-semibold">3 points</span>
-                    </p>
-                    <p>
-                    2 place in event = <span className="font-semibold">2 points</span>
-                    </p>
-                    <p>
-                    3 place in event = <span className="font-semibold">1 point</span>
-                    </p>
-                    </div>
-             
-              </div>
-  )
-}
+    <div className="flex flex-col z-20 items-center justify-center">
+      <h3 className="text-2xl xl:text-3xl font-semibold   text-orange-700">
+        Scoring system
+      </h3>
+      <p className="text-xl text-gray-800 font-semibold capitalize">
+        only the best result counts
+      </p>
 
-export default Scoring
+      <div className="text-xl pt-1 ">
+        <p className="pt-1">
+          1 place in event = <span className="font-semibold">3 points</span>
+        </p>
+        <p>
+          2 place in event = <span className="font-semibold">2 points</span>
+        </p>
+        <p>
+          3 place in event = <span className="font-semibold">1 point</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Scoring;
